87fb501db16eff5ec70bf23494a99ae83e1b074d
Jeff Reback <jeff@reback.net>
2017-03-17 11:28:44 -0400

    CI: typo in using ccache

--- ci/install_travis.sh
+++ ci/install_travis.sh
@@ -78,7 +78,7 @@ fi
 conda info -a || exit 1
 
 # set the compiler cache to work
-if [ "$USE_CACHE" ] && "${TRAVIS_OS_NAME}" == "linux" ]; then
+if [ "$USE_CACHE" ] && [ "${TRAVIS_OS_NAME}" == "linux" ]; then
     echo "[Using ccache]"
     export PATH=/usr/lib/ccache:/usr/lib64/ccache:$PATH
     gcc=$(which gcc)
