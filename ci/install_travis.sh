031958c8a8bba9ec05635080e16c825d89ed7a40
Jeff Reback <jeff@reback.net>
2017-03-17 15:39:53 -0400

    CI: install ccache on osx

--- ci/install_travis.sh
+++ ci/install_travis.sh
@@ -43,6 +43,7 @@ fi
 
 # install miniconda
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    brew install ccache
     time wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh || exit 1
 else
     time wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh || exit 1
